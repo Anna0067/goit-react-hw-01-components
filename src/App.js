3fc530450c6725e8a6ca5781831b88ff772cd3df
Anna0067 <129055045+Anna0067@users.noreply.github.com>
2024-01-17 10:29:51 +0100

    task 1 and 2 - done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-import user from '/user.json';
-import Profile from './Profile';
+import user from './user.json';
+import Profile from './components/Profile';
+import Statistics from './components/Statistics';
+import styles from './components/Statistics.module.css';
 
 const App = () => {
   return (
@@ -12,8 +14,19 @@ const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <div className={styles.app}>
+        <Statistics title="UPLOAD STATS" stats={data} />
+      </div>
     </div>
   );
 };
 
 export default App;
+
+const data = [
+  { id: 'id-1', label: '.docx', percentage: 22 },
+  { id: 'id-2', label: '.pdf', percentage: 4 },
+  { id: 'id-3', label: '.mp3', percentage: 17 },
+  { id: 'id-4', label: '.psd', percentage: 47 },
+  { id: 'id-5', label: '.pdf', percentage: 10 },
+];
---
+++ src/components/Statistics.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './Statistics.module.css';
+
+const generateRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+const Statistics = ({ title, stats }) => {
+  return (
+    <section className={styles.statistics}>
+      {title && <h2 className={styles.title}>{title}</h2>}
+
+      <ul className={styles['stat-list']}>
+        {stats.map(stat => (
+          <li
+            className={styles.item}
+            key={stat.id}
+            style={{ backgroundColor: generateRandomColor() }}
+          >
+            <span className={styles.label}>{stat.label}</span>
+            <span className={styles.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Statistics;
